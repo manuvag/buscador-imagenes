f931d99d8b3d2f8a742566b135673eeca9a9c8c4
Emmanuel Valenzuela <emmanuel@kukulha.com>
2021-11-15 14:41:36 -0600

    Images search application build with react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './content.css'
+import './article.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({photos})
+  return(
+  <div>
+    <header>
+      <Formik
+        initialValues={{ search: '' }}
+        onSubmit={async values => {
+          const response = await fetch(`https://api.unsplash.com/search/photos?per_page=10&query=${values.search}`, {
+            headers: {
+              'Authorization' : 'Client-ID _pqQJNJjkW47cZ88mD_3XvY8CvaIxmXqmTlWTBZIwHg'
+            }
+          })
+
+          const data = await response.json()
+          //Llamar a API de unsplash
+          setPhotos(data.results)  
+        }}
+      >
+        <Form>
+          <Field name="search"></Field>
+        </Form>
+      </Formik>
+    </header>
+    <div className="container">
+      <div className="center">
+        {photos.map(photo => 
+        <article key={photo.id} onClick={() => open(photo.links.html)}>
+          <img src={photo.urls.regular} />
+          <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+        </article>
+        )}
+      </div>
     </div>
-  );
+  </div>
+  )
 }
 
-export default App;
+export default App
